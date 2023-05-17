4229699c64385600c35579802db9bc2f80aebdb6
Devan-Mc <95649823+Devan-Mc@users.noreply.github.com>
2023-05-17 17:20:44 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Project4_XE_Assembler.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/mars5/CLionProjects/Project4-XE-Assembler/main.c" "/mnt/c/Users/mars5/CLionProjects/Project4-XE-Assembler/cmake-build-debug/CMakeFiles/Project4_XE_Assembler.dir/main.c.o"
+  "/mnt/c/Users/mars5/CLionProjects/Project4-XE-Assembler/symbols.c" "/mnt/c/Users/mars5/CLionProjects/Project4-XE-Assembler/cmake-build-debug/CMakeFiles/Project4_XE_Assembler.dir/symbols.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project4_XE_Assembler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project4_XE_Assembler.dir/main.c.o"
+  "CMakeFiles/Project4_XE_Assembler.dir/symbols.c.o"
+  "Project4_XE_Assembler"
+  "Project4_XE_Assembler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project4_XE_Assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
